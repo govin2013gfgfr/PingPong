ebac0653b3ebd0b450ebe3590bdfd7ba97f31d42
govin2013gfgfr <gleb89999408435@gmail.com>
2025-07-07 18:30:17 +0300

    project was created

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+win = pygame.display.set_mode((700, 500))
+
+clock = pygame.time.Clock()
+
+win.fill((0, 220, 255))
+
+is_game = True
+
+while is_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_game = False
+
+    pygame.display.update()
+    clock.tick(40)        
