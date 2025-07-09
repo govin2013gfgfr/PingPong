440ec3b24ab545238433063b193103c3a874a4f3
govin2013gfgfr <gleb89999408435@gmail.com>
2025-07-09 18:12:28 +0300

    'pernul'

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
+
 win = pygame.display.set_mode((700, 500))
 
 clock = pygame.time.Clock()
 
 win.fill((0, 220, 255))
-#俺是个让他
+
 is_game = True
+
+class GameSprite(pygame.sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = pygame.transform.scale(pygame.image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       win.blit(self.image, (self.rect.x, self.rect.y))
 
 while is_game:
     for event in pygame.event.get():
@@ -13,4 +27,4 @@ while is_game:
             is_game = False
 
     pygame.display.update()
-    clock.tick(40)        
+    clock.tick(40)
