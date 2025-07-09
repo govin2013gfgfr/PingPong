217e26a280d6d9ab5da892c66996607ce4acf785
govin2013gfgfr <gleb89999408435@gmail.com>
2025-07-09 18:29:43 +0300

    'pernul'

--- main.py
+++ main.py
@@ -21,6 +21,21 @@ class GameSprite(pygame.sprite.Sprite):
    def reset(self):
        win.blit(self.image, (self.rect.x, self.rect.y))
 
+class player(GameSprite):
+    def update_r(Self)
+        key = pygame.getpressed()
+        if keys[pygame.K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]and self.rect.y < 415:
+            self.rect.y += self.speed
+     def update_l(Self)
+        key = pygame.getpressed()
+        if keys[pygame.K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]and self.rect.y < 415:
+            self.rect.y += self.speed
+
+
 while is_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
